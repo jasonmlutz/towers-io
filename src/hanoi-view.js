14c8698bcae89408f18b35119273e13888682030
Jason Lutz <jason.m.lutz@gmail.com>
2021-01-04 10:33:28 -0800

    Ignore bundle.js; add View class and method skeletons; adjust Game import

--- src/hanoi-view.js
+++ src/hanoi-view.js
@@ -1,1 +1,17 @@
 // hanoi-view.js
+class View {
+  constructor(game, element) {
+    this.game = game;
+    this.element = element;
+    setupTowers();
+    render()
+  }
+
+  setupTowers() = {
+    
+  }
+
+  render() = {
+
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // index.js
 
 import './style.css'
-import './game.js'
+import Game from './game.js'
 import './hanoi-view'
+
+const game = new Game(5);
+console.log(game.towers[0]);
