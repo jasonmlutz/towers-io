53f4c24d403b6f348e7dbfca1a0ab22f9aa00adc
Jason Lutz <jason.m.lutz@gmail.com>
2021-01-04 14:30:44 -0800

    Create game instance and associated view

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@
 import $ from 'jquery'
 import './style.css'
 import Game from './game.js'
-import './hanoi-view'
+import View from './hanoi-view'
+
+const $main = $('.main');
+const game = new Game(5);
+const view = new View(game, $main);
