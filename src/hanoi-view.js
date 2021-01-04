ee4afc49ec0c52be87dcf9648b71120c744a0c69
Jason Lutz <jason.m.lutz@gmail.com>
2021-01-04 14:31:25 -0800

    Complete setupTowers and render methods for View

--- src/hanoi-view.js
+++ src/hanoi-view.js
@@ -1,17 +1,36 @@
 // hanoi-view.js
+import $ from 'jquery'
+
 class View {
   constructor(game, element) {
     this.game = game;
     this.element = element;
-    setupTowers();
-    render()
+    this.setupTowers();
+    this.render();
   }
 
   setupTowers() {
-    
+    for (var i = 0; i < 3; i++) {
+      var tower = $("<ul>")
+      tower.addClass(`tower${i}`);
+      this.element.append(tower);
+    }
   }
 
   render() {
-
+    // for each of the three towers
+    for (var i = 0; i < 3; i++) {
+      // the tower is an array, entries are integers
+      // representing the widths of the discs
+      this.game.towers[i].forEach(width => {
+        var disc = $("<li>");
+        disc.addClass('disc');
+        disc.attr('width', width)
+        disc.text(`${width}`)
+        $(`.tower${i}`).append(disc);
+      });
+    };
   }
 }
+
+export default View
