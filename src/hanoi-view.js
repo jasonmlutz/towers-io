6cc297d7286a98ab1f16c27bd31e674153cebb01
Jason Lutz <jason.m.lutz@gmail.com>
2021-01-08 14:29:16 -0800

    Adjust tower attr; add supporting methods to View

--- src/hanoi-view.js
+++ src/hanoi-view.js
@@ -13,18 +13,20 @@ class View {
     for (var i = 0; i < 3; i++) {
       var tower = $("<ul>")
       tower.addClass(`tower tower${i}`);
+      tower.attr('toweridx', i)
       this.element.append(tower);
     }
   }
 
   render() {
     for (var i = 0; i < 3; i++) {
+      $(`ul[toweridx=${i}]`).empty();
       this.game.towers[i].forEach(width => {
         var disc = $("<li>");
         disc.addClass('disc');
         disc.attr('width', width)
         disc.text(`${width}`)
-        $(`.tower${i}`).append(disc);
+        $(`ul[toweridx=${i}]`).append(disc);
       });
     };
   }
@@ -50,6 +52,8 @@ class View {
       bubbledTarget.removeClass('startTower')
     } else if (this.startTowerSelected()) {
       bubbledTarget.addClass('endTower')
+      this.game.move(...this.movePrep())
+      this.render()
       this.clearTowerFlags()
     } else {
       bubbledTarget.addClass('startTower')
@@ -59,6 +63,17 @@ class View {
   clearTowerFlags() {
     const towers = $('.tower')
     towers.removeClass('startTower endTower')
+  }
+
+  fetchTower(className) {
+    const tower = $(`.${className}`)
+    return tower.attr('toweridx')
+  }
+
+  movePrep() {
+    const startTowerIdx = this.fetchTower('startTower')
+    const endTowerIdx = this.fetchTower('endTower')
+    return [parseInt(startTowerIdx), parseInt(endTowerIdx)]
   }
 }
 
