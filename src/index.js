7f5f3dc805c39285e269b54b5104396b9a127aca
Jason Lutz <jason.m.lutz@gmail.com>
2021-01-11 11:07:23 -0800

    Clean up line lengths and comments

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import View from './hanoi-view'
 
 const $main = $('.main');
 const $buttons = $('.buttons')
-const heightInput = $('<input>').addClass('heightInput')
-const heightInputButton = $('<button>').addClass('heightInputButton')
+const heightInput = $('<input>')
+heightInput.addClass('heightInput')
+const heightInputButton = $('<button>')
+heightInputButton.addClass('heightInputButton')
 heightInputButton.text('input game height')
 $buttons.append(heightInput)
 $buttons.append(heightInputButton)
@@ -20,6 +22,3 @@ heightInputButton.click(function(event) {
     view.towerSelection(event)
   })
 })
-// const game = new Game(3);
-// const view = new View(game, $main);
-// add click event to towers
